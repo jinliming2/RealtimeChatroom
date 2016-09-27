fd1083122406594e3f37f1303da2267a28487ead
Liming Jin <jinliming2@gmail.com>
2016-09-27 19:43:36 +0800

    Test style.👮

--- test/util.test.js
+++ test/util.test.js
@@ -5,57 +5,59 @@
 let mocha = require('mocha');
 let describe = mocha.describe;
 let it = mocha.it;
-let expect = require('chai').expect;
+let assert = require('chai').assert;
 let test = require('../controller/util');
 
 describe('htmlEntities', () => {
     it('i > 1 && j < 2 => i &gt; 1 &amp;&amp; j &lt; 2', () => {
-        return expect(test.htmlEntities('i > 1 && j < 2')).equal('i &gt; 1 &amp;&amp; j &lt; 2');
+        assert.equal(test.htmlEntities('i > 1 && j < 2'), 'i &gt; 1 &amp;&amp; j &lt; 2');
     });
     it('A programmer said: "PHP is the best programming language in the world!" => A programmer said: &quot;PHP is the best programming language in the world!&quot;', () => {
-        return expect(test.htmlEntities('A programmer said: "PHP is the best programming language in the world!"'))
-            .equal('A programmer said: &quot;PHP is the best programming language in the world!&quot;');
+        assert.equal(
+            test.htmlEntities('A programmer said: "PHP is the best programming language in the world!"'),
+            'A programmer said: &quot;PHP is the best programming language in the world!&quot;'
+        );
     });
 });
 
 describe('normalizePort', () => {
     it('(int)80 => 80', () => {
-        return expect(test.normalizePort(80)).equal(80);
+        assert.strictEqual(test.normalizePort(80), 80);
     });
     it('(int)3000 => 3000', () => {
-        return expect(test.normalizePort(3000)).equal(3000);
+        assert.strictEqual(test.normalizePort(3000), 3000);
     });
     it("(int)-1 => false", () => {
-        return expect(test.normalizePort(-1)).false;
+        assert.strictEqual(test.normalizePort(-1), false);
     });
     it("(string)'80' => 80", () => {
-        return expect(test.normalizePort('80')).equal(80);
+        assert.strictEqual(test.normalizePort('80'), 80);
     });
     it("(string)'3000' => 3000", () => {
-        return expect(test.normalizePort('3000')).equal(3000);
+        assert.strictEqual(test.normalizePort('3000'), 3000);
     });
     it("(string)'-1' => false", () => {
-        return expect(test.normalizePort('-1')).false;
+        assert.strictEqual(test.normalizePort('-1'), false);
     });
 });
 
 describe('formatWidth', () => {
     it('(1, 2, "0") => "01"', () => {
-        return expect(test.formatWidth(1, 2, "0")).equal("01");
+        assert.equal(test.formatWidth(1, 2, "0"), "01");
     });
     it('(12, 2, "0") => "12"', () => {
-        return expect(test.formatWidth(12, 2, "0")).equal("12");
+        assert.equal(test.formatWidth(12, 2, "0"), "12");
     });
     it('(2016, 2, "0") => "2016"', () => {
-        return expect(test.formatWidth(2016, 2, "0")).equal("2016");
+        assert.equal(test.formatWidth(2016, 2, "0"), "2016");
     });
     it('(0, 3, "0") => "000"', () => {
-        return expect(test.formatWidth(0, 3, "0")).equal("000");
+        assert.equal(test.formatWidth(0, 3, "0"), "000");
     });
     it('(66, 3, "0") => "066"', () => {
-        return expect(test.formatWidth(66, 3, "0")).equal("066");
+        assert.equal(test.formatWidth(66, 3, "0"), "066");
     });
     it('(789, 3, "0") => "789"', () => {
-        return expect(test.formatWidth(789, 3, "0")).equal("789");
+        assert.equal(test.formatWidth(789, 3, "0"), "789");
     });
 });
